1466e42739b08db07cb19197173f6a4850da3f2f
Yoithebrain <chri86j1@stud.kea.dk>
2018-11-16 12:11:03 +0100

    Added the already made index page to views folder

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
  
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 })
 
 
