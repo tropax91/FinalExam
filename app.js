113f2149b2575c8a2e2c67d179996993781f262b
Yoithebrain <chri86j1@stud.kea.dk>
2018-11-22 12:25:08 +0100

    datasebase connection added to app.js

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 var express = require('express')
 var app = express()
- 
+var app = Handlebars();
+var config = require('../BachelorProject/config/database.js')
+var connection = mysql.createConnection(config.databaseConfig);
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 })
 
 
-
   
-
-
-
-
-
-
-
-
- 
 app.listen('3000', function(err){
     if(err){
         console.log("Couldn't connect to the server");
