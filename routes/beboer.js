a9a3c9e84933b0a734452906fd89afb7911d9a1e
Yoithebrain <chri86j1@stud.kea.dk>
2018-11-22 12:51:59 +0100

    added new stuff to this

--- routes/beboer.js
+++ routes/beboer.js
@@ -1,9 +1,17 @@
 var occupantName = "";
 var occupantId;
 var occupantRole;
-var buildingId;
-var username = "";
-var password = "";
+var occupantbuildingId;
+var occupantusername = "";
+var occupantpassword = "";
 
 
-function beboer()
+function Beboer (name, id, role, buildingnr, username, password){
+    this.occupantName = name;
+    this.occupantId = id;
+    this.occupantRole = role;
+    this.occupantbuildingId = buildingnr;
+    this.occupantusername = username;
+    this.occupantpassword = password;
+}
+
