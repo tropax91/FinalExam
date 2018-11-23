19a03d37d3952846ceaf978233bc554eb43eca22
Yoithebrain <chri86j1@stud.kea.dk>
2018-11-23 13:48:31 +0100

    Added some sql stuff as a skeleton

--- routes/beboer.js
+++ routes/beboer.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+let sql = "";
 
 router.get('/testing.html', function(req, res){
     res.sendFile(__dirname + 'testing.html');
@@ -20,6 +21,29 @@ function Beboer (name, id, role, buildingnr, username, password){
     this.occupantbuildingId = buildingnr;
     this.occupantusername = username;
     this.occupantpassword = password;
+}
+
+//Simple function to send our new beboer to the database
+function sendToDB(){
+    
+    //The sql satenent
+    sql = "CALL createNewUser(?, ?, ?, ?)"
+    
+    //Query
+    dbconnection.query(sql, ['Add value here', 'Add value here', 'Add value here'], function(err, result){
+        if(err) throw err
+        else {
+            console.log(result);
+        }
+    })
+    
+}
+
+//Function to get a user or all users from db
+function getUsers() {
+    
+
+    
 }
 
 module.exports = router;
