39a45b45b990c16a8d1de28b58c870f65c72638f
Yoithebrain <chri86j1@stud.kea.dk>
2018-12-03 13:20:37 +0100

    Added functions and other stuff

--- routes/Reservation.js
+++ routes/Reservation.js
@@ -1,3 +1,10 @@
+const express = require('express');
+const router = express.Router();
+let sql = "";
+
+
+
+
 var date;
 var timeperiodStart;
 var timeperiodEnd;
@@ -13,4 +20,28 @@ function activateReservation () {
     if(timeperiodStart == Date.now() && timeperiodEnd > Date.now()){
         activated = true;
     }
+}
+
+/*This needs to moved somewhere else due to the fact that our reservations will have to be automatically created 
+every 2 months by the system itself*/
+function createReservation(){
+    sql = "CALL createNewReservation(?, ?)"
+
+    dbconnection.query(sql, ['Value here', 'Value here'], function(err, result){
+        if(err){
+            console.log("Something went wrong when creating new reservation");
+        }
+        console.log(result);
+    })
+}
+/* Funktion til at updatere en reservation for at tilføre en bruger til den*/ 
+function reserveReservation(){
+    sql = "CALL reserveReservation (?, ?)"
+
+    dbconnection.query(sql, ['value 1', 'value 2'], function(err, result){
+        if(err){
+            console.log("Error reserve_Reservation");
+        }
+        console.log(result);
+    })
 }
