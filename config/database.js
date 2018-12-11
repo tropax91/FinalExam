58fd93a3581b8dee576cbec0d958fbca86a01cae
Yoithebrain <chri86j1@stud.kea.dk>
2018-12-11 11:11:43 +0100

    Config changed too, to mongoose

--- config/database.js
+++ config/database.js
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-var databaseConfig = {
-    host : '35.187.67.40',
-    database : 'laundryservice',
-    user : 'ext_User',
-    password : 'user1234',
-    port : 3306
-    };
-    module.exports = {databaseConfig : databaseConfig};
-=======
 module.exports = {
     database: 'mongodb://admin:CLC123456@ds127604.mlab.com:27604/vaskeri',
     secret: 'yoursecret'
 }
->>>>>>> casper
